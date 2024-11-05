58e0a18d919b9e643631851fac0eaac416b1c453
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2024-11-05 18:52:15 +0100

    fix minimal

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { config } from './config.js';
 import { paymentRoutes } from './routes/payment.routes.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import morgan from 'morgan';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,7 +17,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
-
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
